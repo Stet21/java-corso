09c0611210c2358e97a8c0307665a17f9442c5c5
Stet21 <melchiorrericcistefano@gmail.com>
2026-02-11 15:57:34 +0100

    prova committ

--- Main.java
+++ Main.java
@@ -4,10 +4,32 @@ public class Main {
 
     public static void main(String[] args) {
      
-      
-}
+      String[][] classi = new String[3][3];
+
+      classi[0][0] = "Luca";
+      classi[0][1] = "Marta";
+      classi[0][2] = "Giovanni";
+      classi[1][0] = "Luca";
+      classi[1][1] = "Marta";
+      classi[1][2] = "Giovanni";
+      classi[2][0] = "Luca";
+      classi[2][1] = "Marta";
+      classi[2][2] = "Giovanni";
+
+      System.out.println(classi[2][1]);
+
+      for(int classe = 0; classe < classi.length; classe++){
+        System.out.println();
+
+         for(int studente = 0; studente < classi[classe].length; studente++ ){
+
+           System.out.print(classi[classe][studente] + " ");
+         }
+
+      }
+  
 }
 
-
+}
 
 
