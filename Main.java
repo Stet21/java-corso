107d3784b9d8667f4a6a556f2bb4d9b57955abc8
Stet21 <melchiorrericcistefano@gmail.com>
2026-02-11 17:15:16 +0100

    corso minuti 4:24:10

--- Main.java
+++ Main.java
@@ -1,34 +1,93 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-     
-      String[][] classi = new String[3][3];
 
-      classi[0][0] = "Luca";
-      classi[0][1] = "Marta";
-      classi[0][2] = "Giovanni";
-      classi[1][0] = "Luca";
-      classi[1][1] = "Marta";
-      classi[1][2] = "Giovanni";
-      classi[2][0] = "Luca";
-      classi[2][1] = "Marta";
-      classi[2][2] = "Giovanni";
+      Persona persona1 = new Persona();
+      Persona persona2 = new Persona();
 
-      System.out.println(classi[2][1]);
 
-      for(int classe = 0; classe < classi.length; classe++){
-        System.out.println();
+      persona1.nome = "Giovanni";
+      persona1.cognome = "Rossi";
 
-         for(int studente = 0; studente < classi[classe].length; studente++ ){
-
-           System.out.print(classi[classe][studente] + " ");
-         }
-
-      }
+      persona1.saluta();
+      persona1.cammina();
   
-}
+      persona2.nome = "Gigi";
+      persona2.cognome = "bianchi";
+
+
+
+
+
+    } 
+    
+    /* overloaded Methods usare lo stesso metondo che usiamo in casistiche diverse
+      praticemnte puopi chiamare la fdunzione in maniera uguale ma se utilizzi paramentri diversi o numero di parametri divero
+      o tipo diverso di dato ( es int o double) possiamo chiamre la funzione nella stessa maniera */
+    
+     /*  static int addizione(int a, int b){
+       int risultato = a + b;
+       return risultato;
+   }
+   static int addizione(int c, int d, int e){
+       int risultato = c + d;
+       return risultato;
+   }
+     */
+    
+    /* arraylist 2d */
+    /*ArrayList<ArrayList<Integer>> classi = new ArrayList();
+    
+    ArrayList<Integer> classe1 = new ArrayList<Integer>();
+
+    classe1.add(1);
+    classe1.add(2);
+    classe1.add(3);
+
+    ArrayList<Integer> classe2 = new ArrayList<Integer>();
+
+    classe2.add(3);
+    classe2.add(4);
+    classe2.add(5);
+    
+    classi.add(classe1);
+    classi.add(classe2);
+
+    for( int i = 0; i< classi.size(); i++){
+       System.out.println();
+
+        for(int j = 0; j <classi.get(i).size(); j++){
+          System.out.print(classi.get(i).get(j) + " ");
+        }
+
+    }*/
+    /* prova funzione addizione */
+    /* int totaleSpeso = addizione(23, 4);
+    int provaFunzione = addizione(totaleSpeso, 25);
+    System.out.println(totaleSpeso);
+    System.out.println(provaFunzione); */
+
+
+    
+
+   /* funzioni o metodi ( differenza concettuale) void funzione che non trorna nessun risultato*/ 
+
+   /* static  void faiPasta(){
+   
+    System.out.println("faiPasta");
+     
+   } */
+  /* Se vuoi che un data esca da una funzione devi escludere il void ( altrimenti non ritornerà nessun data)
+  e inserire static (tipo di dato ad es, int, string ecc) e nome fuznione ( aggiungere parametri che sono dei segnaposti che permettono di inserire dati diversi)
+  i parametri devono essere sempre dello stesso tipo indicato dopo lo static*/
+
+  /*  static int addizione(int a, int b){
+       int risultato = a + b;
+       return risultato;
+   } */
 
 }
 
---
+++ Persona.java
@@ -0,0 +1,24 @@
+public class Persona {
+
+    String nome = "Stefano";
+    String cognome= "Melchiorre Ricci";
+    int eta = 34;
+    double altezza = 1.82;
+
+    void saluta(){
+        System.out.println("ciao sono: " + nome);
+    
+    }
+
+    void cammina(){
+        System.out.println("sto camminando...");
+    }
+    
+}
+
+
+/* Introduzione programmazione ad oggetti */
+
+/* ogni coisa che esiste nel mondo puo essere un oggeto, come una persona, un insieme di persone formano una classe
+un metodo è una funzione usata dentro ad una classe ma siccome java è un liguaggio che utilizza solo classi per lavorare
+ogni funzione è un metodo */
